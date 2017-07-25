f6bdaec01a5d314460f99bc3f439d1c44dcaada5
季伯文 <jibowen@jibowendeMacBook-Pro.local>
2017-07-25 21:26:17 +0800

    立项

---
+++ 计步通/计步通/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  计步通
+//
+//  Created by 季伯文 on 2017/7/25.
+//  Copyright © 2017年 bowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 计步通/计步通/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  计步通
+//
+//  Created by 季伯文 on 2017/7/25.
+//  Copyright © 2017年 bowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
